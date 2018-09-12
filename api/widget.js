9722ea2b24349eab6853fb19795d25cdce5ea370
Lexa M <lexa-m@users.noreply.github.com>
2018-09-12 17:08:48 +0700

    Mistype fixed.

--- api/widget.js
+++ api/widget.js
@@ -253,7 +253,7 @@ ethplorerWidget = {
     // Use local path for develop instances
     fixPath: function(){
         if((document.location.host !== 'ethplorer.io') && (document.location.host.indexOf('ethplorer') >= 0)){
-            //ethplorerWidget.api = '//' + document.location.host + '/api';
+            ethplorerWidget.api = '//' + document.location.host + '/api';
             ethplorerWidget.url = '//' + document.location.host;
         }
     },
